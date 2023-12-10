e14624beb0e45503e7ca4a7a4844cd1a8f7c9379
Sebiten <sebdevspace@gmail.com>
2023-12-09 21:43:47 -0300

    willseetomorrow

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
-import './globals.css'
-import Navbar from './components/Navbar'
-import MyProfilePic from './components/MyProfilePic'
+import "./globals.css";
+import Navbar from "./components/Navbar";
+import { Metadata } from "next";
 
-export const metadata = {
-  title: "Dave's Blog",
-  description: 'Created by Dave Gray',
-}
+export const metadata: Metadata = {
+  title: "Abelardo Blog",
+  description: "Created by Sebastian Burgos",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className="dark:bg-slate-800">
         <Navbar />
-        <MyProfilePic />
-        {children}
+        <main className="px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
+          {children}
+        </main>
       </body>
     </html>
-  )
+  );
 }
--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,57 +1,66 @@
-import getFormattedDate from "@/lib/getFormattedDate"
-import { getSortedPostsData, getPostData } from "@/lib/posts"
-import { notFound } from "next/navigation"
-import Link from "next/link"
+import getFormattedDate from "@/lib/getFormattedDate";
+import { getPostsMeta, getPostByName } from "@/lib/posts";
+import { notFound } from "next/navigation";
+import Link from "next/link";
 
-export function generateStaticParams() {
-    const posts = getSortedPostsData()
+export const revalidate = 0;
 
-    return posts.map((post) => ({
-        postId: post.id
-    }))
-}
+type Props = {
+  params: {
+    postId: string;
+  };
+};
 
-export function generateMetadata({ params }: { params: { postId: string } }) {
+// export async function generateStaticParams() {
+//     const posts = await getPostsMeta()
+//     if (!posts) return []
 
-    const posts = getSortedPostsData()
-    const { postId } = params
+//     return posts.map((post) => ({
+//         postId: post.id
+//     }))
+// }
 
-    const post = posts.find(post => post.id === postId)
+export async function generateMetadata({ params: { postId } }: Props) {
+  const post = await getPostByName(`${postId}.mdx`); //deduped!
 
-    if (!post) {
-        return {
-            title: 'Post Not Found'
-        }
-    }
-
+  if (!post) {
     return {
-        title: post.title,
-    }
+      title: "Post Not Found",
+    };
+  }
+
+  return {
+    title: post.meta.title,
+  };
 }
 
-export default async function Post({ params }: { params: { postId: string } }) {
+export default async function Post({ params: { postId } }: Props) {
+  const post = await getPostByName(`${postId}.mdx`); //deduped!
 
-    const posts = getSortedPostsData()
-    const { postId } = params
+  if (!post) notFound();
 
-    if (!posts.find(post => post.id === postId)) notFound()
+  const { meta, content } = post;
 
-    const { title, date, contentHtml } = await getPostData(postId)
+  const pubDate = getFormattedDate(meta.date);
 
-    const pubDate = getFormattedDate(date)
+  const tags = meta.tags.map((tag, i) => (
+    <Link key={i} href={`/tags/${tag}`}>
+      {tag}
+    </Link>
+  ));
 
-    return (
-        <main className="px-6 prose prose-xl prose-slate dark:prose-invert mx-auto">
-            <h1 className="text-3xl mt-4 mb-0">{title}</h1>
-            <p className="mt-0">
-                {pubDate}
-            </p>
-            <article>
-                <section dangerouslySetInnerHTML={{ __html: contentHtml }} />
-                <p>
-                    <Link href="/">← Back to home</Link>
-                </p>
-            </article>
-        </main>
-    )
+  return (
+    <>
+      <h2 className="text-3xl mt-4 mb-0">{meta.title}</h2>
+      <p className="mt-0 text-sm">{pubDate}</p>
+      <article>{content}</article>
+      <section>
+        <h3>Related:</h3>
+        <div className="flex flex-row gap-4">{tags}</div>
+      </section>
+      <p className="mb-10">
+        <Link href="/">← Back to home</Link>
+      </p>
+    </>
+  );
 }
