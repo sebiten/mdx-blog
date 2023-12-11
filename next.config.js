1e8493a0beb7ad811d5014b3f7cd03c6e6128ddd
Sebiten <sebdevspace@gmail.com>
2023-12-11 13:11:42 -0300

    video-images-mdx

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/sebiten/posts/main/images/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
